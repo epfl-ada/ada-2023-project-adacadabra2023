9d96b9da96d4edacd9d0ad867a85d1f15d928ce2
sandra-heresc <sandra.hernandezescobar@epfl.ch>
2023-12-18 15:34:39 +0100

    web file added

---
+++ file_web.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pandas as pd
+
+st.title(':blue[Beers travelguide]')
+st.write('just for organizational purpose: Exploratory data:')
+
+st.header('Local or industrial?', divider='blue')
+st.write('Proportions of beers that are local and industrial in each country: measurement of locality?')
+
+st.header('Beerstyles?', divider='blue')
+st.write('Preferred beerstyle pie-chart or map with the most popular style per country?')
+
+st.header('Are you generous or picky?', divider='blue')
+st.write('Proportions of beers that are local and industrial in each country: measurement of locality?')
+
+st.header('Seasonal according to beerstyle and country', divider='blue')
+st.write('Country will be modified with an additional tab so that u get 4 pie charts for each country one correponding to each season')
+
+st.header('Map or words per style', divider='blue')
+st.write('most famous beerstyles represented as map of words to check which one is a match with you')
+
+st.write('just for organizational purpose: Interactive data:')
