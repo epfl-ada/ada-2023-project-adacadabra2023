543d95339dc27cde58b4e9aaafdc2e6385819517
CrLou <crloureiro@icloud.com>
2023-12-19 23:14:04 +0100

    new files for webpage

---
+++ HOME.py
@@ -0,0 +1,45 @@
+import streamlit as st
+#import pandas as pd
+
+# Configure page
+st.set_page_config(page_title='Beers Travel Guide', page_icon=':beer_mug:')
+
+# Set Image
+st.image('beer_intro.png')
+
+# Set Title
+st.title('Beers Travel Guide')
+
+# st.sidebar.title("Beers Travel Guide")
+
+st.write(
+    """
+    According to Wikipedia, __beer__ is one of the oldest alcoholic drinks in the world, the most 
+    widely consumed, and the third most popular drink after water and tea. Almost every country in
+    the world, crafts this fermented elixir with their unique blend of ingredients, techniques and 
+    philosophies. Thus it comes with no surprise that beer acts as a portal, inviting us to traverse
+    the diverse cultures, traditions and flavors spanning the globe. 
+    
+    Beer is a universal beverage that gathers people together everywhere around the world. Beer gives 
+    you a reason to make a pause, be it in bars, parks, breweries, homes or vibrant gatherings, where
+    people share stories, forge connections and discuss heterogeneous topics. In essence, beer breeds
+    communities and communities nurture beer.
+
+    In this blog, we set out a beer travel guide for intrepid explorers that provides unique 
+    facets of beer culture around the world. We examine different beer-related features using 
+    beer reviews sourced from beer communities called BeerAdvocate and RateBeer, and provide 
+    traveling recommendations according to the user's needs.
+
+    Join us is if you want to dive into the world of beers, and learn about each country's brewering
+    traditions. Our guide might help you expand your traveling list, or find the perfect destinations
+    aligned with your beer preferences. Isn't this what you beer-loving soul is thirsting for?
+    """
+)
+
+c1, c2 = st.columns(2)
+with c1:
+    st.info('**EPFL - ADA: [@CS401](https://epfl-ada.github.io/teaching/fall2023/cs401/)**', icon="🎓")
+with c2:
+    st.info('**GitHub: [@adacadabra2023](https://github.com/epfl-ada/ada-2023-project-adacadabra2023)**', icon="💻")
+
+st.markdown("Developed by __Sandra Hernández__, __Andrea López__, __Antonello Di Bari__, __Sebastián Deslarzes__ and __Cristina Loureiro__")
---
+++ pages/🏭_Breweries.py
@@ -0,0 +1,108 @@
+# Libraries
+import streamlit as st
+import pandas as pd
+import pickle as pkl
+import numpy as np
+import os
+import plotly.express as px
+import plotly.graph_objects as go
+import plotly.subplots as sp
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import seaborn as sns
+
+# Config
+st.set_page_config(page_title='Beers Travel Guide', page_icon=':beer_mug:')
+
+# Title
+st.title('🏭 What about the breweries?')
+
+st.write(
+    """
+    The reason for this analysis
+    """
+)
+data_exploration, locorind, best_breweries = st.tabs(['Data exploration', 'Local or Industrial?', 'Best breweries to visit'])
+
+# Load datasets
+data_path = os.path.join('/Users/loureiro/Documents/Courses/CS-401_ADA/project_v4_branch/Data', 'Unified_ratings.pkl')
+with open(data_path, 'rb') as f:
+    ratings = pkl.load(f)
+data_path = os.path.join('/Users/loureiro/Documents/Courses/CS-401_ADA/project_v4_branch/Data', 'Unified_breweries.pkl')
+with open(data_path, 'rb') as f:
+    data = pkl.load(f)
+
+with data_exploration:
+    st.write(
+    """
+    Data exploration.....
+    """
+    )
+    
+    # Histograms showing the complete distribution of the number of beers per brewery on the left and the number of breweries producing up to 100 beers on the right
+    fig,axs = plt.subplots(nrows=1, ncols=2, figsize=(14,4), sharey=False, gridspec_kw={'width_ratios':[1,1], 'wspace': 0.5})
+
+    data['total_nbr_beers'].sort_values().plot(kind='hist', bins= 100, ax=axs[0])
+    axs[0].set_xlabel('Number of beers produced by a brewery')
+    axs[0].set_ylabel('Log . Number of breweries')
+    axs[0].set_yscale('log')
+    axs[0].set_title('All breweries')
+
+    data['total_nbr_beers'].sort_values().plot(kind='hist', range = (0, 100), bins= 100, ax=axs[1])
+    axs[1].set_xlabel('Number of beers produced by a brewery')
+    axs[1].set_ylabel('Number of breweries')
+    axs[1].set_title('Breweries with max. 100 beers')
+
+    fig.suptitle('Distribution of breweries producing n types of beers', weight='bold')
+
+    st.pyplot(fig)
+
+with locorind:
+    st.write(
+    """
+    Explain analysis + plot.....
+    """
+    )
+    #Here we define the threshold for local vs industrial breweries
+    # Local breweries < 15
+    # Industrial breweries > 15
+    data['local_brewery'] = data['total_nbr_beers'].apply(lambda x: True if x < 15 else False)
+    
+    # Plotting the proportion of local vs industrial breweries per country
+    # We filtered out countries having less than 30 breweries for better statistics
+    fig, ax = plt.subplots(figsize=(2.1, 6))
+
+    grouped = data.groupby('location').agg({
+        'local_brewery': lambda x: x.mean(),
+        'name': 'size'
+    }).reset_index()
+    grouped.sort_values(by='local_brewery', ascending=False, inplace=True)
+    grouped['industrial_brewery'] = grouped['local_brewery'] - 1
+    grouped = grouped[grouped['name'] > 30]
+
+    plt.rcParams['font.size'] = '6'
+    for label in (ax.get_yticklabels()):
+        label.set_fontsize(5)
+
+    sns.barplot(data=grouped, y='location', x='local_brewery', color='#058872')
+    sns.barplot(data=grouped, y='location', x='industrial_brewery', color='#DAA71A')
+
+    local_patch = mpatches.Patch(color='#058872', label='Local breweries')
+    industrial_patch = mpatches.Patch(color='#DAA71A', label='Industrial breweries')
+    plt.legend(handles=[local_patch, industrial_patch], bbox_to_anchor=(1.05, 0.98), fontsize='6', ncol=1)
+    plt.ylabel('Countries', weight='bold')
+    plt.xlabel('Proportion of industrial vs local breweries', weight='bold')
+    #plt.legend(labels=["Local breweries","Industrial breweries"], loc='upper left', labelcolor=['#058872', '#DAA71A'], fontsize='small')
+    plt.xlim(-1.2,1.2)
+    st.pyplot(fig)
+
+with best_breweries:
+    option = st.selectbox('Select a country you are interested in visiting?',
+        (np.sort(data['location'].unique())))
+
+    df = ratings[ratings['country_brewery']==option].groupby('brewery_name').agg({'rating':'mean'}).reset_index().sort_values(by='rating', ascending=False)
+ 
+    st.write('You selected the country:', option)
+    st.write('This are the top rated breweries for that country:')
+    st.write(df['brewery_name'].head(5))
+    
